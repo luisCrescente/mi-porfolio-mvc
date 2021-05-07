b55324f4f9a7eae11f254d5b67aea5046f0c9792
luisCrescente <luis94.lc@gmail.com>
2021-05-07 16:22:01 -0300

    router

--- app.js
+++ app.js
@@ -1,20 +1,17 @@
 const express = require ( 'express' );
 const path = require('path');
 const app = express ();
+const rutasMain = require("../mi_porfolio_mvc/routers/main.js")
 
 const publicPath = path.resolve(__dirname,'./public');
 app.use(express.static(publicPath));
 
+app.use("/", rutasMain);
 
-app . listen ( 3004 , () => {
+app.listen ( 3004 , () => {
     console.log( 'Servidor funcionando en el puerto 3004' );
 });
 
-app.get('/',(req,res)=>{
-    res.sendFile(path.resolve(__dirname,'./views/home.html'));
-});
 
 
-app.get('/about',(req,res)=>{
-    res.sendFile(path.resolve(__dirname, 'views/about.html'));
-});
+
